408f8d3036caa1802717004cbb1814263ec5ac4d
npantos17 <72134672+npantos17@users.noreply.github.com>
2025-08-04 03:15:27 +0200

    Initial commit

---
+++ src/main/java/com/example/ecommercebackendtask/ECommerceBackendTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackendTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommercebackendtask/ECommerceBackendTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebackendtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
